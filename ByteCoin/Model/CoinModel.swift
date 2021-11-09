79ded4033b9bc56b8b61dc1867c0beefc60b7990
juanpablosg <juan.sanchezgz@udlap.mx>
2021-11-08 22:23:14 -0600

    Delegate, External API Challenge

---
+++ ByteCoin/Model/CoinModel.swift
@@ -0,0 +1,14 @@
+//
+//  CoinModel.swift
+//  ByteCoin
+//
+//  Created by Juanpablo Sánchez G. on 07/11/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinModel: Decodable {
+    let rate: Double
+    
+}
